60d5e1197b38a0e0d99ea6711da358983b709952
Deepak <62474186+Sdipu@users.noreply.github.com>
2020-10-23 08:53:28 -0700

    Update experiment3prime.java

--- experiment3prime.java
+++ experiment3prime.java
@@ -1,25 +1,44 @@
 import java.util.Scanner;
 
-public class experiment4 {
-    //print prime number
-    public static void main(String[] args){
-        System.out.println("by deepak prime number ");
-        System.out.println("enter the nth number: ");
+
+// Java program to print all primes smaller than or equal to
+// n using Sieve of Eratosthenes
+
+class experiment4
+{
+    public static void main(String args[])
+    {    System.out.println("prime number by deepak");
+        System.out.println("enter a number");
+
         Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print("Following are the prime numbers ");
 
-        int n = sc.nextInt();
-        System.out.println("series: ");
-                for(int i =2;i<n;i++){
-                    boolean isprime = true;
-                    for(int j =2;j<i/2;j++){
-                        if(i%j==0){
-                            isprime = false;
-                            break;
-                        }
-
-                    }
-                    if(isprime) System.out.print(" "+i);
-                }
+        Primenumber(num);
 
     }
+
+    public static void Primenumber(int n)
+    {
+        boolean prime[] = new boolean[n+1];
+        for(int i=0;i<n;i++)
+            prime[i] = true;
+
+        for(int p = 2; p*p <=n; p++)
+        {
+            if(prime[p] == true)
+            {
+                // Update all multiples of p
+                for(int i = p*p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        for(int i = 2; i <= n; i++)
+        {
+            if(prime[i] == true)
+                System.out.print(i + " ");
+        }
+    }
+
 }
+
